5964e92a3d861b86a5121360471cc27b8861f8b9
slim <=>
2017-04-17 21:49:00 +0200

    prepare auth data

---
+++ auth.py
@@ -0,0 +1,15 @@
+import requests
+
+
+CLIENT_ID = 'xxx'
+CLIENT_SECRET = 'xxx'
+
+def get_access_tokens():
+	"""
+	request tokens to spotify
+	check doc to get it through redirect url
+	https://developer.spotify.com/web-api/
+	"""
+
+	return ''
+
